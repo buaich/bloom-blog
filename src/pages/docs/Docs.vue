a11b8e9d935b41ffb7c9a2b2a8caabc4ae2d725f
bloom <2597466845@qq.com>
2026-03-24 16:27:08 +0800

    feat: 对Docs组件进行基本的布局

--- src/pages/docs/Docs.vue
+++ src/pages/docs/Docs.vue
@@ -1,36 +1,104 @@
-<script setup lang="ts" name="Docs"></script>
+<script setup lang="ts" name="Doc">
+import { ref } from "vue";
+
+const list = ref<string[]>();
+list.value = [
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+  "11dkfajsdkfjaksg",
+];
+</script>
 
 <template>
-  <div class="doc">
-    <div class="doc__list"></div>
-    <div class="doc__content"></div>
-    <div class="doc__details"></div>
+  <div class="doc-container">
+    <div class="doc-sidebar">
+      <div
+        v-for="item in list"
+        :key="item"
+        class="doc-sidebar-option"
+        tabindex="0"
+      >
+        {{ item }}
+      </div>
+    </div>
+    <div class="doc-content"></div>
+    <div class="doc-outline"></div>
   </div>
 </template>
 
 <style scoped>
-.doc {
-  --list-width: 250px;
+.doc-container {
+  --doc-sidebar-width: 260px;
+  --doc-outline-width: 240px;
+  --doc-border: 1px solid #e2e8f0;
+  --font-clr: gray;
+  --bg-clr: #f8f9fa;
 }
-.doc {
+.doc-container {
   display: flex;
-  flex-grow: 1;
-  flex-shrink: 1;
-  flex-basis: 0;
-  background-color: black;
-  width: 100%;
   height: 100%;
+  width: 100%;
+  background-color: var(--bg-clr);
+  color: var(--font-clr);
 }
-.doc__list {
-  width: var(--list-width);
-  background-color: aqua;
+
+.doc-sidebar {
+  width: var(--doc-sidebar-width);
+  padding: 1em 2em;
+  border-right: var(--doc-border);
+  overflow-y: auto;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  cursor: pointer;
 }
-.doc__content {
+
+.doc-sidebar-option:hover {
+  color: black;
+}
+.doc-sidebar-option:focus,
+.doc-sidebar-option:active {
+  color: black;
+}
+
+.doc-content {
   flex: 1;
-  background-color: pink;
 }
-.doc__details {
-  width: var(--list-width);
-  background-color: red;
+
+.doc-outline {
+  width: var(--doc-outline-width);
+  border-left: var(--doc-border);
 }
+
+/* @media (max-width: 1024px) {
+  .doc-outline {
+    display: none;
+  }
+}
+@media (max-width: 768px) {
+  .doc-sidebar,
+  .doc-outline {
+    display: none;
+  }
+} */
 </style>
