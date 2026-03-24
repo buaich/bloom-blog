86dd2df909c3b57fdd241d91e7c223f5188b1fd4
bloom <2597466845@qq.com>
2026-03-24 15:31:49 +0800

    chore: 调整一下

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,11 @@ import HomeMenu from "./components/HomeMenu.vue";
 </script>
 
 <template>
-  <div class="root">
+  <div class="app">
     <HomeMenu />
-    <div class="login-part" v-if="!$route.meta.hideDefault">
+    <div class="router-view-wrapper">
       <router-view name="default" />
     </div>
   </div>
 </template>
-
-<style scoped>
-.login-part,
-.note-part {
-  margin-top: 14px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 625px;
-  /* font-size: 15px; */
-  background-color: rgb(243, 246, 249);
-  gap: 14px;
-  box-sizing: border-box;
-  padding: 5px;
-  border-radius: 14px;
-}
-</style>
+<style scoped></style>
