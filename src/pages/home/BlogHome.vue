378bb14750e0768891f90d04480f5cc40cb1fcf3
bloom <2597466845@qq.com>
2026-03-28 11:04:55 +0800

    bug: .dropdown-option父元素中仅将其.dropdown-btn子元素的display设置成none，父元素padding仍然存在，导致总宽度超过弹性容器宽度，在手机端就会溢出，在媒体查询中，直接给父元素隐藏即可

--- src/pages/home/BlogHome.vue
+++ src/pages/home/BlogHome.vue
@@ -1,38 +1,7 @@
-<script setup lang="ts" name="BlogHome">
-import { onMounted, useTemplateRef } from "vue";
-
-const canvasRef = useTemplateRef<HTMLCanvasElement>("canvas");
-
-function draw() {
-  const ctx = canvasRef.value?.getContext("2d");
-  if (!ctx) return;
-
-  ctx.strokeStyle = "black";
-  ctx.beginPath();
-  ctx.moveTo(100, 100);
-  ctx.lineTo(110, 110);
-  ctx.lineTo(120, 110);
-  ctx.lineTo(110, 110);
-  ctx.closePath();
-  ctx.stroke();
-}
-</script>
+<script setup lang="ts" name="BlogHome"></script>
 
 <template>
-  <div class="container">
-    <canvas ref="canvas"></canvas>
-  </div>
+  <div class="container"></div>
 </template>
 
-<style scoped>
-.container {
-  display: flex;
-  width: 100%;
-  color: var(--font-clr-one);
-}
-
-canvas {
-  width: 100%;
-  height: 100%;
-}
-</style>
+<style scoped></style>
