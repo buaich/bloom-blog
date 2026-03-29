04c9796a5c40e1caa7fe78ee0ff1f1f77a551551
bloom <2597466845@qq.com>
2026-03-29 23:05:55 +0800

    fix: 安装SEO相关依赖

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,21 @@ import { defineConfig } from "vitest/config";
 import { fileURLToPath, URL } from "node:url";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import seoPrerender from "vite-plugin-seo-prerender";
 
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    seoPrerender({
+      routes: ["/"],
+    }),
+  ],
 
   test: {
     globals: true,
-    environment: "happy-dom",
+    environment: "happy-dom", // 你之前有这一项，建议保留
   },
-
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
