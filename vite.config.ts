c2f6a8af7113da536d2da0e77a7fbf4484193dac
bloom <2597466845@qq.com>
2026-03-30 10:48:42 +0800

    fix: 修复Vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,31 @@ import { defineConfig } from "vitest/config";
 import { fileURLToPath, URL } from "node:url";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
-
 import { cloudflare } from "@cloudflare/vite-plugin";
+import Markdown from "unplugin-vue-markdown/vite";
+import seoPrerender from "vite-plugin-seo-prerender";
 
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), cloudflare()],
+  plugins: [
+    vue({ include: [/\.vue$/, /\.md$/] }),
+
+    vueDevTools(),
+
+    cloudflare(),
+
+    Markdown({
+      include: [/\.md$/],
+      markdownItOptions: {
+        html: true,
+        linkify: true,
+        typographer: true,
+      },
+    }),
+
+    seoPrerender({
+      routes: ["/"],
+    }),
+  ],
 
   test: {
     globals: true,
